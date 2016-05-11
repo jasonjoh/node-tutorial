cd2d5fa709142671f9890495da038e244db9c4c1
Jason Johnston <jasonjoh@microsoft.com>
2016-05-11 16:56:02 -0400

    Removed extra profile scope
    My last commit introduced an extraneous "profile" scope entry, removing
    it.

--- authHelper.js
+++ authHelper.js
@@ -15,8 +15,7 @@ var scopes = [ "openid",
                "profile", 
                "https://outlook.office.com/mail.read",
                "https://outlook.office.com/calendars.read",
-               "https://outlook.office.com/contacts.read",
-               "profile" ];
+               "https://outlook.office.com/contacts.read" ];
 
 function getAuthUrl() {
   var returnVal = oauth2.authCode.authorizeURL({
