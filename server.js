97e0867950a9379dac7bf9586f047318545a3aa9
burashka <burashka-lcme@yandex.ru>
2017-10-24 17:07:03 +0300

    Move code to ES2017

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
-var http = require('http');
-var url = require('url');
+const http = require('http');
+const url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var pathName = url.parse(request.url).pathname;
-    console.log('Request for ' + pathName + ' received.');
+    const pathName = url.parse(request.url).pathname;
+    console.log(`Request for ${pathName} received.`);
     route(handle, pathName, response, request);
   }
   
-  var port = 8000;
+  const port = 8000;
   http.createServer(onRequest).listen(port);
-  console.log('Server has started. Listening on port: ' + port + '...');
+  console.log(`Server has started. Listening on port: ${port} ...`);
 }
 
 exports.start = start;
